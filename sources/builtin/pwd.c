ffca379004d57d3434303e41291f50f5fdb85c89
huntae-42 <tjung@student.42seoul.kr>
2022-03-04 20:43:24 +0900

    변경사항 무시 - 중요 X

--- sources/builtin/pwd.c
+++ sources/builtin/pwd.c
@@ -6,7 +6,7 @@
 /*   By: tjung <tjung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 12:52:17 by minsunki          #+#    #+#             */
-/*   Updated: 2022/03/03 13:38:53 by tjung            ###   ########.fr       */
+/*   Updated: 2022/03/04 19:24:31 by tjung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,7 @@ int	builtin_pwd(void)
 	m = meta_get();
 	path = getcwd(NULL, 0);
 	if (!path)
-	{
-		m->exit_status = 1;
 		perror_exit("getcwd failed @builtin_pwd");
-	}
 	ft_putendl_fd(path, STDOUT_FILENO);
 	free(path);
 	m->exit_status = 0;
