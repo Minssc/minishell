d1934eaf69f64f9f3c89c61f8cb8bc66822db3ed
huntae-42 <tjung@student.42seoul.kr>
2022-03-11 18:13:37 +0900

    - builtin_exit: child process 에 따라 다른 적용 - minishell.h: norm err 수정

--- sources/builtin/exit.c
+++ sources/builtin/exit.c
@@ -6,28 +6,68 @@
 /*   By: tjung <tjung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 12:52:06 by minsunki          #+#    #+#             */
-/*   Updated: 2022/03/11 13:46:31 by tjung            ###   ########.fr       */
+/*   Updated: 2022/03/11 18:01:29 by tjung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	sub_builtin_exit_perror(int idx, int is_num, char *arg)
+static int	check_rpipe_location(t_meta *m)
 {
-	if (idx == 2 && is_num)
-		ft_putendl_fd("exit", STDOUT_FILENO);
-	else
+	t_token	*ct;
+	t_token	*et;
+
+	ct = m->token_start;
+	et = NULL;
+	while (ct)
 	{
-		ft_putendl_fd("exit", STDERR_FILENO);
-		if (!is_num)
-			ms_puterr(arg, EM_NUMERIC_REQUIR);
-		else
+		if (!ft_strcmp(ct->str, "exit"))
 		{
-			ms_puterr(arg, EM_TOO_MANY_ARGS);
-			return (0);
+			et = ct->next;
+			while (et)
+			{
+				if (et->type == T_PIP)
+					return (1);
+				et = et->next;
+			}
 		}
+		ct = ct->next;
 	}
-	return (1);
+	return (0);
+}
+
+static void	do_exit(int is_pip, int es, char *arg, int opt)
+{
+	t_meta	*m;
+
+	m = meta_get();
+	if ((!m->child && !is_pip) || (m->child && !is_pip))
+	{
+		m->exit_status = es;
+		if (!m->child)
+			ft_putendl_fd("exit", STDERR_FILENO);
+		if (opt == 3)
+			ms_puterr(arg, EM_TOO_MANY_ARGS);
+		else if (opt == 4)
+			ms_puterr(arg, EM_NUMERIC_REQUIR);
+		if (opt != 3)
+			mexit(m->exit_status);
+	}
+}
+
+static void	sub_builtin_exit(t_meta *m, char *arg, int opt)
+{
+	int	is_pip;
+
+	is_pip = check_rpipe_location(m);
+	if (opt == 1)
+		do_exit(is_pip, 0, NULL, 1);
+	else if (opt == 2)
+		do_exit(is_pip, ft_atoi(arg) % 256, NULL, 2);
+	else if (opt == 3)
+		do_exit(is_pip, 1, arg, 3);
+	else if (opt == 4)
+		do_exit(is_pip, 2, arg, 4);
 }
 
 void	builtin_exit(char **exit_cmd)
@@ -40,20 +80,11 @@ void	builtin_exit(char **exit_cmd)
 	while (exit_cmd[idx])
 		idx++;
 	if (idx == 1)
-	{
-		m->exit_status = 0;
-		ft_putendl_fd("exit", STDOUT_FILENO);
-		mexit(m->exit_status);
-	}
+		sub_builtin_exit(m, NULL, 1);
 	else if (idx == 2 && ft_isnumstr(exit_cmd[1]))
-		m->exit_status = ft_atoi(exit_cmd[1]) % 256;
+		sub_builtin_exit(m, exit_cmd[1], 2);
 	else if (idx > 2 && ft_isnumstr(exit_cmd[1]))
-		m->exit_status = 1;
+		sub_builtin_exit(m, exit_cmd[1], 3);
 	else if (!ft_isnumstr(exit_cmd[1]))
-		m->exit_status = 2;
-	else
-		m->exit_status = 255;
-	if (!sub_builtin_exit_perror(idx, ft_isnumstr(exit_cmd[1]), exit_cmd[1]))
-		return ;
-	mexit(m->exit_status);
+		sub_builtin_exit(m, exit_cmd[1], 4);
 }
--- sources/include/minishell.h
+++ sources/include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minsunki <minsunki@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: tjung <tjung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 11:56:27 by minsunki          #+#    #+#             */
-/*   Updated: 2022/03/11 14:59:00 by minsunki         ###   ########seoul.kr  */
+/*   Updated: 2022/03/11 15:34:25 by tjung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -170,7 +170,7 @@ int		parse(t_meta *m, char *line);
 void	ms_skip_quotes(char **str, char quote);
 
 // parse/parse_util.c
-int	sub_parse(t_meta *m, char *line, char *cur);
+int		sub_parse(t_meta *m, char *line, char *cur);
 
 // parse/ourt.c
 void	sort_tokens(t_meta *m);
